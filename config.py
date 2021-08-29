bcff9785eb406a3ee9531ac2d5fc9c82daa07ee5
pythontoday <pythontodaynet@gmail.com>
2021-08-29 14:57:32 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+host = "your_host"
+user = "your_user"
+password = "your_password"
+db_name = "your_db_name"
---
+++ main.py
@@ -0,0 +1,68 @@
+import psycopg2
+from config import host, user, password, db_name
+
+
+try:
+    # connect to exist database
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=db_name    
+    )
+    connection.autocommit = True
+    
+    # the cursor for perfoming database operations
+    # cursor = connection.cursor()
+    
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT version();"
+        )
+        
+        print(f"Server version: {cursor.fetchone()}")
+        
+    # create a new table
+    # with connection.cursor() as cursor:
+    #     cursor.execute(
+    #         """CREATE TABLE users(
+    #             id serial PRIMARY KEY,
+    #             first_name varchar(50) NOT NULL,
+    #             nick_name varchar(50) NOT NULL);"""
+    #     )
+        
+    #     # connection.commit()
+    #     print("[INFO] Table created successfully")
+        
+    # insert data into a table
+    # with connection.cursor() as cursor:
+    #     cursor.execute(
+    #         """INSERT INTO users (first_name, nick_name) VALUES
+    #         ('Oleg', 'barracuda');"""
+    #     )
+        
+    #     print("[INFO] Data was succefully inserted")
+        
+    # get data from a table
+    # with connection.cursor() as cursor:
+    #     cursor.execute(
+    #         """SELECT nick_name FROM users WHERE first_name = 'Oleg';"""
+    #     )
+        
+    #     print(cursor.fetchone())
+        
+    # delete a table
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """DROP TABLE users;"""
+        )
+        
+        print("[INFO] Table was deleted")
+    
+except Exception as _ex:
+    print("[INFO] Error while working with PostgreSQL", _ex)
+finally:
+    if connection:
+        # cursor.close()
+        connection.close()
+        print("[INFO] PostgreSQL connection closed")
